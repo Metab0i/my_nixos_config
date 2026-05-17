57ca2ecc2a8bf75529b275ad2c107bc4d78371a7
Metab0i <metaboi0000@gmail.com>
2026-05-17 17:29:32 +1000

    Initial commit of my nixos config

---
+++ configuration.nix
@@ -0,0 +1,214 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+
+
+  # Bootloader:
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+
+  # Networking
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+
+
+  # Timezone & Region Configuration:
+
+  # Set your time zone.
+  #time.timeZone = en.wikipedia.org/wiki/List_of_tz_database_time_zones;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+
+
+  # Desctop Configuration:
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+
+
+  # Printing: Enable CUPS to print documents.
+  services.printing.enable = true;
+
+
+
+  # Sound Configuration: 
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+
+
+  # Users & Groups
+
+  # Define users
+  users.users = {
+    metab0i = {
+      isNormalUser = true;
+      description = "metab0i";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [
+        # pckg..
+      ];
+    };
+    agent0 = {
+      isNormalUser = true;
+      description = "Meant to be used by automated harnessed predictive models and similar processes";
+      packages = with pkgs; [
+	#claude-code
+	opencode		
+      ];
+    };
+  };
+
+  # Define Groups:
+  users.groups = {
+    shared_access = {
+      gid = 1001;
+      members = [ "agent0" "metab0i" ];
+    };
+  };
+
+
+
+  # Packages:
+
+  # Using proprietary
+  nixpkgs.config.allowUnfree = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    pciutils
+    brave
+    rclone
+    rclone-ui
+    mullvad-vpn
+    gnomeExtensions.appindicator
+  ];
+
+  # Mullvad VPN config
+  services.mullvad-vpn.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+
+
+  # Hardware Config - Hybrid GPU
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["modesetting" "nvidia"];
+  hardware.nvidia = {
+    # for gpu state snapshotting on transition from sleep to wake and vice-versa
+    powerManagement.enable = true;
+
+    # Enable nvidia settings menu.
+    # Available via nvidia-settings
+    nvidiaSettings = true;
+
+    # Driver source
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    # Don't use opensource drivers
+    open = false;
+
+    # Hybrid GPUs set-up
+    modesetting.enable = true;
+    prime = {
+      sync.enable = true;
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+}
