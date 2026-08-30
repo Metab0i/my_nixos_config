bdfa4de9f34d1460f6af5088ef86118e676bfcff
Metab0i <metaboi0000@gmail.com>
2026-08-30 19:49:38 +1000

    Enabled flakes and nix-command, as well as upgraded stable source to 26.05

--- configuration.nix
+++ configuration.nix
@@ -231,6 +231,14 @@ in
   # Mullvad VPN config
   services.mullvad-vpn.enable = true;
 
+
+
+  # Experimental Features, enable nix-command and flakes
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -256,7 +264,7 @@ in
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 
 
 
